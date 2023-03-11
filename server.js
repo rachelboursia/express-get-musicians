99e77c15608824d47dee37436e9f378da2b338cc
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2023-03-11 13:09:20 -0800

    updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//app.use('/musicians', require('./routes/musicians'));
+
 app.get("/musicians", async (req, res) => {
     const musicians = await Musician.findAll();
     res.json(musicians);
 });
 
+app.get("/musicians/:id", async (req, res) => {
+    const musician = await Musician.findByPk(req.params.id);
+    res.json(musician);
+});
+
+  
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
